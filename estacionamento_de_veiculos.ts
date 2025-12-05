b2b21453433afa2ad36e8dd1d3f09f9b78cbe729
Lichia <alicia.dota@aluno.ufr.edu.br>
2025-12-05 16:02:41 -0400

    Update - Esqueleto da estrutura (com erros)

--- estacionamento_de_veiculos.ts
+++ estacionamento_de_veiculos.ts
@@ -4,11 +4,11 @@ enum TipoVeiculo {
     Truck = 'Caminhao'
 }
 
-interface Preco{
+interface Estrategia_Preco{
     calcule(veiculo: Veiculo, horas_totais: number): number;
 }
 
-abstract class Veiculo implements Preco{
+abstract class Veiculo{
     private placa: string;
     private tipo: TipoVeiculo;
 
@@ -16,12 +16,80 @@ abstract class Veiculo implements Preco{
         this.placa = placa;
         this.tipo = tipo;
     }
+
+    getPlaca(): string {
+        return this.placa;
+    }
     
-    calcule(veiculo: Veiculo, horas_totais: number): number {
-        let preco_base = this.getBasePricePerHour(0, horas_totais);
+    getTipo(): TipoVeiculo {
+        return this.tipo;
     }
 
     getBasePricePerHour(hora_inicio: number, hora_final: number): number {
         
     }
+}
+
+class Local_Vaga{
+    private id: number;
+    private allowedType: TipoVeiculo;
+
+    constructor(id: number, allowedType: TipoVeiculo){
+        this.id = id;
+        this.allowedType = allowedType;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getAllowedType(): TipoVeiculo {
+        return this.allowedType;
+    }
+
+    isFree(): boolean {
+
+    }
+
+    park(vehicle: Veiculo): boolean {
+        //só estaciona se estiver livre e vehicle.type === allowedType
+    }
+
+    leave(): Veiculo | null {
+
+    }
+}
+
+class Lista_Vagas{
+    private l_vagas: Veiculo[] = [];
+
+    addSpot(vaga: Local_Vaga): void{
+
+    }
+
+    findFreeSpotFor(v: Veiculo): Local_Vaga | undefined {
+
+    }
+
+    park(v: Veiculo): Local_Vaga | undefined {
+
+    }
+
+    leaveFromSpot(id: string): Veiculo | null {
+
+    }
+
+    calculateFee(v: Veiculo, h: number, e: Estrategia_Preco): number {
+
+    }
+}
+
+function FlatRatePricing(): {
+    //cobra horas arredondadas × preço base
+}
+function ProgressivePricing(): {
+    //primeiras N horas preço normal, depois fator maior
+}
+function OvernightPricing(): {
+    //acima de certo número de horas, aplica tarifa noturna especial
 }
